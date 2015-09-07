88be7201427c5c9b51837b4f70dd2ff0109f5990
Nate Brennand <natebrennand@gmail.com>
2015-09-06 21:44:44 -0700

    pool connections & add list endpoint

--- db/redis.go
+++ db/redis.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"fmt"
+	"log"
 	"time"
 
 	"github.com/garyburd/redigo/redis"
@@ -16,12 +17,30 @@ func ns(slug string) string {
 	return namespace + "." + slug
 }
 
+// Redis is a wrapper over a Redis pool connection.
 type Redis struct {
-	c redis.Conn
+	p *redis.Pool
 }
 
+// NewRedisDB connects to Redis and pools connections.
+func NewRedisDB() ShortenBackend {
+	redisURL := getOrDefault("REDIS_URL", "localhost:6379")
+	pool := newPool(redisURL)
+	conn, err := pool.Dial()
+	if err != nil {
+		log.Fatalf("Failed to connect to redis @ '%s': %s", redisURL, err)
+	}
+	conn.Close()
+
+	return Redis{p: pool}
+}
+
+// DeleteURL removes all reference to a URL.
 func (r Redis) DeleteURL(slug string) error {
-	n, err := redis.Int(r.c.Do("DEL", slug))
+	c := r.p.Get()
+	defer c.Close()
+
+	n, err := redis.Int(c.Do("DEL", slug))
 	if err != nil {
 		return fmt.Errorf("Failed to delete '%s': %s", slug, err)
 	} else if n != 1 {
@@ -31,9 +50,13 @@ func (r Redis) DeleteURL(slug string) error {
 	return nil
 }
 
+// ShortenURL adds a URL object to the db.
 func (r Redis) ShortenURL(slug, longURL, owner string, expires time.Time) error {
+	c := r.p.Get()
+	defer c.Close()
+
 	// save as a hash
-	_, err := r.c.Do("HMSET", ns(slug),
+	_, err := c.Do("HMSET", ns(slug),
 		"long_url", longURL,
 		"owner", owner)
 	if err != nil {
@@ -41,7 +64,7 @@ func (r Redis) ShortenURL(slug, longURL, owner string, expires time.Time) error 
 	}
 
 	// set the expire time
-	n, err := redis.Int(r.c.Do("PEXPIREAT", ns(slug), expires.Unix()))
+	n, err := redis.Int(c.Do("PEXPIREAT", ns(slug), expires.Unix()))
 	if err != nil {
 		return fmt.Errorf("Failed to set expire time for '%s': %s", slug, err)
 	} else if n == 0 {
@@ -51,8 +74,12 @@ func (r Redis) ShortenURL(slug, longURL, owner string, expires time.Time) error 
 	return nil
 }
 
+// GetLongURL searches for the short URL reference in order to return the long url.
 func (r Redis) GetLongURL(slug string) (string, error) {
-	longURL, err := redis.String(r.c.Do("HGET", ns(slug), "long_url"))
+	c := r.p.Get()
+	defer c.Close()
+
+	longURL, err := redis.String(c.Do("HGET", ns(slug), "long_url"))
 	if err != nil {
 		return "", fmt.Errorf("Failed to find long_url for '%s': %s", slug, err)
 	} else if longURL == "" {
@@ -62,7 +89,59 @@ func (r Redis) GetLongURL(slug string) (string, error) {
 	return longURL, nil
 }
 
+// GetList lists all shortened URLs.
 func (r Redis) GetList() ([]ShortenObject, error) {
-	// redis.Strings("")
+	c := r.p.Get()
+	defer c.Close()
+
+	// get the list of every URL hash
+	urlKeys, err := redis.Strings(c.Do("KEYS", ns("*")))
+	if err != nil {
+		return []ShortenObject{}, fmt.Errorf("Failed to scan to find all records: %s", err)
+	}
+
+	// pipeline retrieving every
+	for _, key := range urlKeys {
+		c.Send("HGET", key, "slug", "owner", "long_url")
+	}
+
+	// flush all commands
+	err = c.Flush()
+	if err != nil {
+		return []ShortenObject{}, fmt.Errorf("Failed to flush query for list: %s", err)
+	}
+
+	objs := make([]ShortenObject, len(urlKeys))
+	for i := range objs {
+		url, err := redis.Values(c.Receive())
+		if err != nil {
+			return []ShortenObject{}, fmt.Errorf("Failed to recieve value for list: %s", err)
+		}
+
+		u := objs[i]
+		_, err = redis.Scan(url, &u.Slug, &u.Owner, &u.LongURL)
+		if err != nil {
+			return []ShortenObject{}, fmt.Errorf("Failed to marshal value: %s", err)
+		}
+	}
+
 	return []ShortenObject{}, nil
+}
+
+func newPool(server string) *redis.Pool {
+	return &redis.Pool{
+		MaxIdle:     3,
+		IdleTimeout: 240 * time.Second,
+		Dial: func() (redis.Conn, error) {
+			c, err := redis.Dial("tcp", server)
+			if err != nil {
+				return nil, err
+			}
+			return c, err
+		},
+		TestOnBorrow: func(c redis.Conn, t time.Time) error {
+			_, err := c.Do("PING")
+			return err
+		},
+	}
 }

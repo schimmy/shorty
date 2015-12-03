c35c3699f0f65237932ef280ba1f38bc687c6ff5
Nate Brennand <nate.brennand@clever.com>
2015-12-03 22:58:00 +0000

    cleaner drone config

--- integration_tests.sh
+++ integration_tests.sh
@@ -6,9 +6,7 @@ export PG_USER=postgres
 export PG_HOST=localhost
 export PG_DATABASE=shortener
 
+createdb -h "$PG_HOST" -U "$PG_USER" "$PG_DATABASE"
 psql -U $PG_USER -d $PG_DATABASE -h "$PG_HOST" < ./pg_schema.sql
 
 make test
-
-# kill all child processes to clean up - pkill always returns nonzero
-pkill -P $$ || echo "test complete"
